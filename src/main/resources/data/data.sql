170c528cf8801b15d6822ca1195b9e0e73f94348
Professor <mh571536@gmail.com>
2024-06-03 17:52:07 +0500

    Edit Some Basic Configuration

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (id, user_id, first_name, last_name, email, phone, bio, reference_id, image_url, created_by, updated_by, created_at,updated_at,account_non_expired,account_non_locked,enabled,mfa)
-VALUES (0, '023a7479-e7a7-3ae5-a766fe25eca9', 'System', 'Jamia', 'system@gmail.com', '123456789', 'This is not user but system itself.', '8972578-e7a7-3ae5-a766fe25eca9', 'https://image.com', 0, 0, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,true,true,false,false);
+VALUES (0, '023a7479-e7a7-3ae5-a766fe25eca9', 'System', 'Spring Security', 'system@gmail.com', '123456789', 'This is not user but system itself.', '8972578-e7a7-3ae5-a766fe25eca9', 'https://image.com', 0, 0, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,true,true,false,false);
